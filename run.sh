4b2b539e2cbf3b2f54916607f3ce0adde6af4ce6
crimera <>
2024-06-03 09:57:43 +0800

    improve run script

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-php -S 127.0.0.1:8000 &  cd server; sh run.sh & 
+export C_INCLUDE_PATH="$HOME/dev/whisper.cpp"
+export LIBRARY_PATH="$HOME/dev/whisper.cpp"
+
+php -S 127.0.0.1:8000 &
+cd server; air & 
 cd ../
 npx tailwindcss -i styles/index.css -o dist/out.css --watch
--- server/run.sh
+++
@@ -1,1 +0,0 @@
-export C_INCLUDE_PATH="$HOME/dev/whisper.cpp"; export LIBRARY_PATH="$HOME/dev/whisper.cpp"; air
